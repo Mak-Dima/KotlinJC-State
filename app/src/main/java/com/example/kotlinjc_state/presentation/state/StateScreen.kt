00a9a9d116502dd4a3db6acddfa233c8296a2f98
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-02-23 19:03:01 -0600

    Counter mutable state added.

--- app/src/main/java/com/example/kotlinjc_state/presentation/state/StateScreen.kt
+++ app/src/main/java/com/example/kotlinjc_state/presentation/state/StateScreen.kt
@@ -7,12 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 
 @Composable
 fun StateScreen(innerPadding: PaddingValues) {
+
+    var counter by remember { mutableIntStateOf(1) }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -20,7 +27,7 @@ fun StateScreen(innerPadding: PaddingValues) {
             .padding(innerPadding)
             .fillMaxSize()
     ) {
-        Text("Hello")
+        Text("Counter: $counter")
     }
 }
 
